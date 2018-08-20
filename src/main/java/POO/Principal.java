c7c8e4bea0f2d7ee88b9712757669dfa2c05e177
Guilhermao <guilhermefliegerfelipe@gmail.com>
2018-08-20 13:24:40 -0300

    Merging exercício 2

--- src/main/java/POO/Principal.java
+++ src/main/java/POO/Principal.java
@@ -2,7 +2,12 @@ package POO;
 
 public class Principal {
     public static void main(String[] args) {
+        Exercicio01 ex1 = new Exercicio01();
+        ex1.criaRetangulo(5);
+
+        System.out.println();
+
         Exercicio02 ex2 = new Exercicio02();
-        ex2.criaLosango(10);
+        ex2.criaLosango(7);
     }
 }
