4762ecf4e123ad7bc27476cd00d4fe0c3b188605
Guilhermao <guilhermefliegerfelipe@gmail.com>
2018-08-20 13:07:35 -0300

    Linhas do losango concluídas.

--- src/main/java/POO/Exercicio02.java
+++ src/main/java/POO/Exercicio02.java
@@ -8,13 +8,25 @@ public class Exercicio02 {
     }
 
 
+    private void linha(int espacamento){
+        for(int j = 0; j <= espacamento; j++){
+            if(j == 0 || j == espacamento) {
+                System.out.print("*");
+            } else{
+                System.out.print(" ");
+            }
+        }
+        System.out.println();
+    }
 
     public void criaLosango(int h){
         for(int i = 0; i < h/2; i++){
             nEspacos(h/2-i);
+            linha(2*i);
         }
         for(int i = h/2; i >= 0; i--){
             nEspacos(h/2-i);
+            linha(2*i);
         }
     }
 }
